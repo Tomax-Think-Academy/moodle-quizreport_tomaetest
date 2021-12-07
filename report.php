f5f85b6670a90c556afc5751bff7c931b6dd50d3
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-07 15:59:04 +0200

    adding copyright and licensing info

--- report.php
+++ report.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * version.php - version information.
+ *
+ * @package    quiz_tomaetest
+ * @subpackage quiz
+ * @copyright  2021 Tomax ltd <roy@tomax.co.il>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -31,13 +53,13 @@ class quiz_tomaetest_report extends quiz_default_report
         $this->print_header_and_tabs($cm, $course, $quiz, 'archive');
         if (quizaccess_tomaetest_utils::getMoodleTeachers($quiz->id, $USER->id)) {
 
-            
+
 
             $this->display_archive($quiz);
             return true;
         }else{
             echo "<p> <b>No permissions to view this page.</b></p>";
-            
+
         }
     }
 
