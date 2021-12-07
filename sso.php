035f6ad979f679825954c7c20659bcaa023ee890
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-07 16:12:45 +0200

    Variable "parID" must be all lower-case

--- sso.php
+++ sso.php
@@ -29,7 +29,7 @@ require_once($CFG->dirroot . "/mod/quiz/accessrule/tomaetest/rule.php");
 global $DB, $CFG, $USER;
 
 $id = $_GET["id"];
-$parID = isset($_GET["parID"]) ? $_GET["parID"] : null;
+$parid = isset($_GET["parID"]) ? $_GET["parID"] : null;
 
 
 $users = quizaccess_tomaetest_utils::getMoodleTeachers($id, $USER->id);
@@ -45,7 +45,7 @@ if (!empty($adminUser) || !empty($users)) {
 }
 // if it is admin user.
 if (!empty($adminUser)) {
-    //If it cant SSO, create the user, and then continue to SSO.
+    // If it cant SSO, create the user, and then continue to SSO.
     $data = quizaccess_tomaetest_utils::createSystemUser($USER->id);
     if ($data !== true) {
         echo "<script>alert('$data')</script>";
@@ -53,7 +53,7 @@ if (!empty($adminUser)) {
         exit;
     }
 }
-$result = tomaetest_connection::sso($id, $USER->id, $parID);
+$result = tomaetest_connection::sso($id, $USER->id, $parid);
 
 if ($result === false) {
     // Check if admin priviliges
