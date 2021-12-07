01d6e3c202d8c6449bcd68d27e5d360a4e44f486
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-07 16:36:43 +0200

    remove comments and fix indentation

--- report.php
+++ report.php
@@ -43,8 +43,7 @@ class quiz_tomaetest_report extends quiz_default_report
     protected $quiz;
     protected $course;
 
-    public function display($quiz, $cm, $course)
-    {
+    public function display($quiz, $cm, $course) {
         global $PAGE, $USER;
         $this->quiz = $quiz;
         $this->cm = $cm;
@@ -53,11 +52,9 @@ class quiz_tomaetest_report extends quiz_default_report
         $this->print_header_and_tabs($cm, $course, $quiz, 'archive');
         if (quizaccess_tomaetest_utils::getMoodleTeachers($quiz->id, $USER->id)) {
 
-
-
             $this->display_archive($quiz);
             return true;
-        }else{
+        } else {
             echo "<p> <b>No permissions to view this page.</b></p>";
 
         }
@@ -66,22 +63,21 @@ class quiz_tomaetest_report extends quiz_default_report
     /**
      * Display all attempts.
      */
-    protected function display_archive($quiz)
-    {
-        global $OUTPUT, $PAGE,$USER;
+    protected function display_archive($quiz) {
+        global $OUTPUT, $PAGE, $USER;
         if (quizaccess_tomaetest_utils::isETestPluginEnabled()) {
 
             $record = quizaccess_tomaetest_utils::get_etest_quiz($quiz->id);
             if ($record != false) {
-                $loginToPanel = new moodle_url('/mod/quiz/report/tomaetest/sso.php', array('id' => $this->quiz->id));
-                $loginToGrade = new moodle_url('/mod/quiz/report/tomaetest/ssoTG.php');
-                echo "<a target='_blank' href='$loginToPanel'>Click here to start TomaETest Monitor</a></br>";
+                $logintopanel = new moodle_url('/mod/quiz/report/tomaetest/sso.php', array('id' => $this->quiz->id));
+                $logintograde = new moodle_url('/mod/quiz/report/tomaetest/ssoTG.php');
+                echo "<a target='_blank' href='$logintopanel'>Click here to start TomaETest Monitor</a></br>";
                 $extradata = $record->extradata;
-                 if(isset ($extradata["IDMatch"]) && $extradata["IDMatch"] === true && isset($extradata["TeacherID"])){
-                     if ($USER->id === $extradata["TeacherID"]){
-                        echo "<a target='_blank' href='$loginToGrade'>Click here to start TomaGrade.</a></br>";
-                     }
-                 };
+                if(isset ($extradata["IDMatch"]) && $extradata["IDMatch"] === true && isset($extradata["TeacherID"])){
+                    if ($USER->id === $extradata["TeacherID"]){
+                        echo "<a target='_blank' href='$logintograde'>Click here to start TomaGrade.</a></br>";
+                    }
+                };
                 // var_dump()
                 echo "<p>The Exam Code is <b>" . $record->extradata["TETExamLink"] . "</b> .";
                 $table = new html_table();
@@ -92,26 +88,22 @@ class quiz_tomaetest_report extends quiz_default_report
                 $table->head = ["Participant", "Integrity score"];
                 foreach ($info["data"]["participants"] as $participant) {
                     $score = "Waiting for score";
-                    $participantID = $participant["ParticipantID"];
+                    $participantid = $participant["ParticipantID"];
                     if (isset($participant["parExamAtts"]["TETExamParticipantSystemScore"]["key"])) {
                         $score = $participant["parExamAtts"]["TETExamParticipantSystemScore"]["key"];
                     }
                     if (isset($participant["parExamAtts"]["TETExamParticipantUserScore"]["key"])) {
                         $score = $participant["parExamAtts"]["TETExamParticipantUserScore"]["key"];
                     }
-                    $loginToParticipant = new moodle_url('/mod/quiz/report/tomaetest/sso.php', array('id' => $this->quiz->id, 'parID' => $participantID));
-                    $score = "<a target='_blank' href='$loginToParticipant'>$score</a>";
+                    $logintoparticipant = new moodle_url('/mod/quiz/report/tomaetest/sso.php', array('id' => $this->quiz->id, 'parID' => $participantid));
+                    $score = "<a target='_blank' href='$logintoparticipant'>$score</a>";
                     $table->data[] = array($participant["UserName"], $score);
-                    //$participant["UserName"]
-                    // Low
                 }
                 echo html_writer::table($table);
                 return;
-                // exit;
             }
             echo "<p>The plugin is not enabled or the exam does not have TomaETest Proctoring enabled.</p>";
         }
-        // echo "<a href=''>Click here to see advanced integrity report</a>";
     }
 
     protected function quizreportgetstudentandattempts($quiz) {
