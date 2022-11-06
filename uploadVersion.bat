99cae059c70d523501e05b5ee21e2b53475ffcad
Guy Sinter <guy.s@tomadox.co.uk>
2022-11-06 15:07:41 +0200

    modify bucket

--- uploadVersion.bat
+++ uploadVersion.bat
@@ -1,6 +1,6 @@
 
-aws s3 cp ./TET-report-moodle.zip s3://capsule.public/MoodlePlugins/ETest/TET-report-moodle.zip
-aws s3api put-object-acl --bucket capsule.public --key MoodlePlugins/ETest/TET-report-moodle.zip --acl public-read
+aws s3 cp ./TET-report-moodle.zip s3://downloads-tomax-io/MoodlePlugins/ETest/TET-report-moodle.zip
+aws s3api put-object-acl --bucket downloads-tomax-io --key MoodlePlugins/ETest/TET-report-moodle.zip --acl public-read
 
-echo You can test the link now - https://s3.eu-west-1.amazonaws.com/capsule.public/MoodlePlugins/ETest/TET-report-moodle.zip
+echo You can test the link now - https://s3.eu-west-1.amazonaws.com/downloads-tomax-io/MoodlePlugins/ETest/TET-report-moodle.zip
 pause
